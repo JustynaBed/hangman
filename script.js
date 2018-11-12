c4700bbdfa4aa6e85dacf3f958dffb56ec067768
JustynaBed <justyna.bednarko@gmail.com>
2018-11-12 14:22:05 +0100

    render keyboard

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const hangmanPlace = document.getElementById('hangman-place');
 const keyboardPlace = document.getElementById('keyboard-place');
 
 
-let words = ["jogging", "joking", "jukebox", "frazzled", "juicy", "pneumonia", "yippee",
+const words = ["jogging", "joking", "jukebox", "frazzled", "juicy", "pneumonia", "yippee",
     "zilch", "peekaboo", "jelly", "avenue", "axiom", "unknown unknown", "whiskey whiskey",
     "unworthy", "thumbscrew", "twelfth"];
 
+const letters = ["A", "Ą", "B", "C", "Ć", "D", "E",
+"Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N",
+"Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U",
+"V", "W", "X", "Y", "Z", "Ź", "Ż"];
 
 function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
@@ -15,7 +19,7 @@ function getRandomWord() {
 
 let hangmanWord = getRandomWord().toUpperCase();
 console.log('%c hangmanWord: ', 'color: green', hangmanWord)
-console.log('%c hangmanWord: ', 'color: green', hangmanWord.length)
+console.log('%c hangmanWordLength: ', 'color: green', hangmanWord.length)
 
 let wordToGuess = "";
 
@@ -33,6 +37,18 @@ function getWord() {
 
 window.onload = start;
 
+
 function start() {
+    let render_keyboard = '';
+
+    letters.map(i => {
+        let element = 'lit' + letters.indexOf(i);
+        render_keyboard +=
+        `
+        <div class="letter active" onclick="alert(${letters.indexOf(i)})" id="${element}">${i}</div>
+        `
+    });
+    keyboardPlace.innerHTML = render_keyboard;
     getWord();
 }
+
